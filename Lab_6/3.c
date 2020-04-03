dcf97aaa53aefa117a7095b62af45c897e580088
kryptoTomo <tomasz.madej101@gmail.com>
2020-04-03 12:42:14 +0200

    new

--- Lab_6/3.c
+++ Lab_6/3.c
@@ -10,38 +10,52 @@ int i_rand(int min, int max)
 void proc_1(int * a, int * b,int n);
 int main()
 {
-  int x;
-  printf("------------------------------\nPodaj lp przesunięć\n");
-  scanf("%d",&x);
-  int tab_1[5] = {1,2,3,4,5};
-  proc_1(tab_1,tab_1+4,x);
-  printf("\nTab_1\n");
-    wypisz_i (tab_1,tab_1+4);
-  printf("------------------------------\nTablica Dynamiczna\n");
-  int N,M; //N - ilość wierszy   M- ilość kolumn 
-  printf("Podaj Ilość wierszy i kolumn:");
-  scanf("%d %d",&N,&M);
-  printf("------------------------------\nPodaj lp przesunięć\n");
-  scanf("%d",&x);
-  int** tab;
-  tab = calloc(N,sizeof(int*));
-  *tab = calloc(N*M,sizeof(int));
-  for(int i = 1;i<N;i++)
-    *(tab + i) = i*M+*tab;
-  srand(time(0));
-  printf("Wylosowane Wartości:\n");
-  for (int i = 0; i < N; i++ ) 
-      for (int j = 0; j < M; j++ )
-      { 
-        tab[i][j] = i_rand(0,14);
-      } 
-  for (int i = 0; i < N; i++ ) 
-    wypisz_i (*(tab+i),*(tab +i)+M-1);
-  proc_1(&(tab[0][0]),&(tab[N-1][M-1]),1); 
-  printf("Tablica po przesunięciu:\n");
-  for (int i = 0; i < N; i++ ) 
-    wypisz_i (*(tab+i),*(tab +i)+M-1);
+  	int x;
+  	int tab_1[5] = {1,2,3,4,5};
+  	printf("------------------------------\ntab_1 przed przesunięciem:\n");
+  	wypisz_i (tab_1,tab_1+4);
+  	printf("Podaj lp przesunięć:\n");  	
+  	scanf("%d",&x);
+  	proc_1(tab_1,tab_1+4,x);
+  	printf("\ntab_1 po przesunięciu:\n");
+  	wypisz_i (tab_1,tab_1+4);
+  	
 
+  	int tab_2 [4][2] = {{10,11},{12,13},{14,15},{16,17}};
+  	printf("------------------------------\ntab_2 przed przesunięciem:\n");
+  	for (int i = 0; i < 4; i++ ) 
+  		wypisz_i (*(tab_2+i),*(tab_2 +i)+1);
+  	printf("Podaj lp przesunięć:\n");
+  	scanf("%d",&x);
+  	proc_1(*tab_2,*tab_2+7,x);
+	printf("tab_2 po przesunięciu:\n");
+  	for (int i = 0; i < 4; i++ ) 
+    	wypisz_i (*(tab_2+i),*(tab_2 +i)+1);
+  	
+
+  	printf("------------------------------\nTablica Dynamiczna\n");
+  	int N,M; //N - ilość wierszy   M- ilość kolumn 
+  	printf("Podaj Ilość wierszy i kolumn:");
+  	scanf("%d %d",&N,&M);
+  	printf("Podaj lp przesunięć:\n");
+  	scanf("%d",&x);
+  	int** tab;
+  	tab = calloc(N,sizeof(int*));
+  	*tab = calloc(N*M,sizeof(int));
+  	for(int i = 1;i<N;i++)
+  	   	*(tab + i) = i*M+*tab;
+  	srand(time(0));
+  	printf("Wylosowane Wartości:\n");
+  	for (int i = 0; i < N; i++ ) 
+    	for (int j = 0; j < M; j++ )
+        	tab[i][j] = i_rand(0,14); 
+  	for (int i = 0; i < N; i++ ) 
+    	wypisz_i (*(tab+i),*(tab +i)+M-1);
+  	proc_1(&(tab[0][0]),&(tab[N-1][M-1]),x); 
+  	printf("Tablica po przesunięciu:\n");
+  	for (int i = 0; i < N; i++ ) 
+    	wypisz_i (*(tab+i),*(tab +i)+M-1);
+    printf("------------------------------\n");
 
   return 0;
 }
